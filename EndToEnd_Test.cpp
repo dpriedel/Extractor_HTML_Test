99d7deaa7b228271151481e32e58c148d3b9228a
David P. Riedel <davidp.riedel@gmail.com>
2019-07-13 13:55:19 -0400

    add tests for HTML export

--- EndToEnd_Test.cpp
+++ EndToEnd_Test.cpp
@@ -269,6 +269,59 @@ TEST_F(SingleFileEndToEnd_HTML, VerifyCanLoadDataToDBForFileWithHTML_10QHTML)
 
 	ASSERT_EQ(CountRows(), 80);
 }
+
+TEST_F(SingleFileEndToEnd_HTML, ExportSingleFile)
+{
+	//	NOTE: the program name 'the_program' in the command line below is ignored in the
+	//	the test program.
+
+	std::vector<std::string> tokens{"the_program",
+        "--log-level", "debug",
+		"--form", "10-Q,10-K",
+        "--mode", "HTML",
+		"--log-path", "/tmp/test1.log",
+//		"--list", "./list_with_bad_file.txt"
+        "--export-HTML-data",
+        "--HTML-forms-to-dir", "/tmp/extracts",
+        "--HTML-forms-from-dir", "/vol_DA/SEC/Archives/edgar",
+        "--file", "/vol_DA/SEC/Archives/edgar/data/google-10k.txt"
+    };
+
+	try
+	{
+        ExtractorApp myApp(tokens);
+
+		decltype(auto) test_info = UnitTest::GetInstance()->current_test_info();
+        spdlog::info(catenate("\n\nTest: ", test_info->name(), " test case: ",
+                test_info->test_case_name(), "\n\n"));
+
+        bool startup_OK = myApp.Startup();
+        if (startup_OK)
+        {
+            myApp.Run();
+            myApp.Shutdown();
+        }
+        else
+        {
+            std::cout << "Problems starting program.  No processing done.\n";
+        }
+	}
+
+    // catch any problems trying to setup application
+
+	catch (const std::exception& theProblem)
+	{
+        spdlog::error(catenate("Something fundamental went wrong: ", theProblem.what()));
+		throw;	//	so test framework will get it too.
+	}
+	catch (...)
+	{		// handle exception: unspecified
+        spdlog::error("Something totally unexpected happened.");
+		throw;
+	}
+	ASSERT_EQ(CountRows(), 0);
+}
+
 //TEST_F(SingleFileEndToEnd, VerifyCanLoadDataToDBForFileWithXML_NoNamespace_10Q)
 //{
 //	//	NOTE: the program name 'the_program' in the command line below is ignored in the
@@ -654,47 +707,57 @@ TEST_F(ProcessFolderEndtoEnd, UseDirectory_10Q_HTML)
 //	ASSERT_EQ(CountFilings(), 31);
 //}
 //
-//TEST_F(ProcessFolderEndtoEnd, WorkWithFileListContainsBadFile)
-//{
-//	//	NOTE: the program name 'the_program' in the command line below is ignored in the
-//	//	the test program.
-//
-//	std::vector<std::string> tokens{"the_program",
-//        "--log-level", "debug",
-//		"--form", "10-Q,10-K",
-//		"--log-path", "/tmp/test1.log",
-//		"--list", "./list_with_bad_file.txt"
-//    };
-//
-//    ExtractorApp myApp;
-//	try
-//	{
-//        myApp.init(tokens);
-//
-//		decltype(auto) test_info = UnitTest::GetInstance()->current_test_info();
-//		myApp.logger().information(std::string("\n\nTest: ") + test_info->name() + " test case: " + test_info->test_case_name() + "\n\n");
-//
-//        myApp.run();
-//	}
-//
-//    // catch any problems trying to setup application
-//
-//	catch (const std::exception& theProblem)
-//	{
-//		// poco_fatal(myApp->logger(), theProblem.what());
-//
-//		myApp.logger().error(std::string("Something fundamental went wrong: ") + theProblem.what());
-//		throw;	//	so test framework will get it too.
-//	}
-//	catch (...)
-//	{		// handle exception: unspecified
-//		myApp.logger().error("Something totally unexpected happened.");
-//		throw;
-//	}
-//    // there are 45 potential filings in the list.  3 are 'bad'.
-//	ASSERT_EQ(CountFilings(), 42);
-//}
-//
+TEST_F(ProcessFolderEndtoEnd, WorkWithFileListContainsBadFileForExport)
+{
+	//	NOTE: the program name 'the_program' in the command line below is ignored in the
+	//	the test program.
+
+	std::vector<std::string> tokens{"the_program",
+        "--log-level", "debug",
+		"--form", "10-Q,10-K",
+        "--mode", "HTML",
+		"--log-path", "/tmp/test1.log",
+		"--list", "./list_with_bad_file.txt",
+        "--export-HTML-data",
+        "--HTML-forms-to-dir", "/tmp/extracts",
+        "--HTML-forms-from-dir", "/vol_DA/SEC"
+    };
+
+	try
+	{
+        ExtractorApp myApp(tokens);
+
+		decltype(auto) test_info = UnitTest::GetInstance()->current_test_info();
+        spdlog::info(catenate("\n\nTest: ", test_info->name(), " test case: ",
+                test_info->test_case_name(), "\n\n"));
+
+        bool startup_OK = myApp.Startup();
+        if (startup_OK)
+        {
+            myApp.Run();
+            myApp.Shutdown();
+        }
+        else
+        {
+            std::cout << "Problems starting program.  No processing done.\n";
+        }
+	}
+
+    // catch any problems trying to setup application
+
+	catch (const std::exception& theProblem)
+	{
+        spdlog::error(catenate("Something fundamental went wrong: ", theProblem.what()));
+		throw;	//	so test framework will get it too.
+	}
+	catch (...)
+	{		// handle exception: unspecified
+        spdlog::error("Something totally unexpected happened.");
+		throw;
+	}
+	ASSERT_EQ(CountFilings(), 42);
+}
+
 //TEST_F(ProcessFolderEndtoEnd, WorkWithMissingValuesFileList1)
 //{
 //	//	NOTE: the program name 'the_program' in the command line below is ignored in the
